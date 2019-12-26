30ba7892f0874f012216603b190bce78a2c66d13
gleydisson <gleydisson.bona@gmail.com>
2019-12-26 17:55:05 -0500

    Build com seguranca listedDomains

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://financial-money-api.herokuapp.com'
+  apiUrl: 'https://financial-money-api.herokuapp.com',
+  tokenWhitelistedDomains: [ 'financial-money-api.herokuapp.com' ],
+  tokenBlacklistedRoutes: [ 'https://financial-money-api.herokuapp.com/oauth/token' ]
 };
